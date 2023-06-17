17bd30466707d3e9e400d7845c5044d7a54c3522
Mohammed Eid <mohammedeidprog@gmail.com>
2023-06-18 01:21:28 +0300

    modifying the db uri3

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const dbConnection = () => {
-  mongoose.connect(process.env.DB_URI).then((conn) => {
-    console.log(`Database connected: ${conn.connection.host}`);
-  });
+  mongoose
+    .connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then((conn) => {
+      console.log(`Database connected: ${conn.connection.host}`);
+    });
 };
 
 module.exports = dbConnection;
