9529041999bf907c904ed356eb60544dea57ed57
Mohammed Eid <mohammedeidprog@gmail.com>
2023-06-18 01:07:54 +0300

    modify the db url

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const dbConnection = () => {
-  mongoose.connect(process.env.DB_URI).then((conn) => {
-    console.log(`Database connected: ${conn.connection.host}`);
-  });
+  mongoose
+    .connect(
+      "mongodb://meid:ERRvjTBwmqHCv5Lr@ac-xg9vuv0-shard-00-00.pn9yzom.mongodb.net:27017,ac-xg9vuv0-shard-00-01.pn9yzom.mongodb.net:27017,ac-xg9vuv0-shard-00-02.pn9yzom.mongodb.net:27017/udemy-ecommerce?ssl=true&replicaSet=atlas-pj61g9-shard-0&authSource=admin&retryWrites=true&w=majority"
+    )
+    .then((conn) => {
+      console.log(`Database connected: ${conn.connection.host}`);
+    });
 };
 
 module.exports = dbConnection;
