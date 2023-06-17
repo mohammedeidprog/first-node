db599379c25a64ff71fbe6ef88b63388530483e7
Mohammed Eid <mohammedeidprog@gmail.com>
2023-06-18 01:46:19 +0300

    modifying the db uri 6

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = () => {
   mongoose
-    .connect(process.env.DB_URI, {
+    .connect("", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
