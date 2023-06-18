b87f7b101c81bc16d7b75b397063f1d91a6feb69
Mohammed Eid <mohammedeidprog@gmail.com>
2023-06-18 15:17:06 +0300

    editing the database link

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbConnection = () => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect("", {
+    .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
